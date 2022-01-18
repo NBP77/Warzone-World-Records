5f0abea46d3ee1977cc6e1e0c46dc622f29eca57
NBP77 <nbp77@outlook.com>
2022-01-18 12:45:00 -0500

    Getting format of record cards right

--- src/Components/Records/solos.js
+++ src/Components/Records/solos.js
@@ -6,23 +6,21 @@ import "bootstrap/dist/css/bootstrap.min.css";
 function SolosV() {
   const DisplaySolosV = worldRecordsVerdansk.slice(0, 1).map((obj) => {
     return (
-      <div>
-        <div className="record-card">
-          <div className="recordType">{obj.record}</div>
-          <img src={obj.picture} alt="iNewwz the gamer" />
-          <div>{obj.name}</div>
-          <div>{obj.kills}</div>
-          <iframe
-            width="350"
-            height="200"
-            src={obj.gameplay}
-            title="YouTube video player"
-            frameborder="0"
-            allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture"
-            allowfullscreen
-          ></iframe>
-          <div>{obj.date}</div>
-        </div>
+      <div className="record-card">
+        <div className="recordType">{obj.record}</div>
+        <img className="inewwz-pic" src={obj.picture} alt="iNewwz the gamer" />
+        <div className="gamers-name">{obj.name}</div>
+        <div className="kills">{obj.kills}</div>
+        <div className="date">{obj.date}</div>
+        <iframe
+          width="350"
+          height="200"
+          src={obj.gameplay}
+          title="YouTube video player"
+          frameborder="0"
+          allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture"
+          allowfullscreen
+        ></iframe>
       </div>
     );
   });
